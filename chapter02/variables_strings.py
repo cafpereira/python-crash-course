3eb206c7869b3ae68018912c004f36397ff799a1
cpereira <cpereira@groupon.com>
2018-02-03 19:32:06 -0500

    starting chapter 2

---
+++ chapter02/variables_strings.py
@@ -0,0 +1,26 @@
+message = "Hello Python Crash Course reader!"
+print(message)
+
+name = "ada lovelace"
+print(name.title())
+print(name.upper())
+print(name.lower())
+
+first_name = "ada"
+last_name = "lovelace"
+full_name = first_name + " " + last_name
+print(full_name)
+
+print("Hello, " + full_name.title() + "!")
+
+print("Languages:\n\tPython\n\tC\n\tJavaScript")
+
+favorite_language = ' python '
+print('>>>' + favorite_language + '<<<')
+print('>>>' + favorite_language.rstrip() + '<<<')
+print('>>>' + favorite_language.lstrip() + '<<<')
+print('>>>' + favorite_language.strip() + '<<<')
+
+# message = 'One of Python's strengths is its diverse community.'
+message = "One of Python's strengths is its diverse community."
+print(message)
