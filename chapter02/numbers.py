5f8a6036c59083a4fd25ddfcf6656377b9906d31
Carlos Pereira <carlos_pereira@ultimatesoftware.com>
2018-02-05 13:28:31 -0500

    integers and floats

--- chapter02/numbers.py
+++ chapter02/numbers.py
@@ -1,1 +1,14 @@
-(2 + 3) * 4
+print((2 + 3) * 4)
+# >>> 20
+print(2 * 0.1)
+# >>> 0.2
+print(0.2 * 0.1)
+# >>> 0.020000000000000004
+print(3/2)
+# >>> 1.5
+
+age = 23
+message = "Happy " + str(age) + "rd Birthday!"
+print(message)
+
+import this
