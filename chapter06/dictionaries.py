96a6a81d82611d78f5eb9f83cd05194009fbe573
Carlos Pereira <carlos_pereira@ultimatesoftware.com>
2018-02-07 18:59:43 -0500

    chapter 6: dictionaries

---
+++ chapter06/dictionaries.py
@@ -0,0 +1,119 @@
+alien_0 = {'color': 'green', 'points': 5}
+print(alien_0['color'])
+print(alien_0['points'])
+
+new_points = alien_0['points']
+print("You just earned " + str(new_points) + " points!")
+
+alien_0['x_position'] = 0
+alien_0['y_position'] = 25
+print(alien_0)
+
+alien_0 = {'x_position': 0, 'y_position': 25, 'speed': 'medium'}
+print("Original x-position: " + str(alien_0['x_position']))
+# Move the alien to the right.
+# Determine how far to move the alien based on its current speed.
+if alien_0['speed'] == 'slow':
+    x_increment = 1
+elif alien_0['speed'] == 'medium':
+    x_increment = 2
+else:
+    # This must be a fast alien.
+    x_increment = 3
+# The new position is the old position plus the increment.
+alien_0['x_position'] = alien_0['x_position'] + x_increment
+print("New x-position: " + str(alien_0['x_position']))
+
+alien_0 = {'color': 'green', 'points': 5}
+print(alien_0)
+
+del alien_0['points']
+print(alien_0)
+
+favorite_languages = {
+    'jen': 'python',
+    'sarah': 'c',
+    'edward': 'ruby',
+    'phil': 'python',
+}
+print("Sarah's favorite language is " +
+      favorite_languages['sarah'].title() +
+      ".")
+
+user_0 = {
+    'username': 'efermi',
+    'first': 'enrico',
+    'last': 'fermi',
+}
+for key, value in user_0.items():
+    print("\nKey: " + key)
+    print("Value: " + value)
+
+for name in favorite_languages.keys():
+    print(name.title())
+
+friends = ['phil', 'sarah']
+for name in favorite_languages.keys():
+    print(name.title())
+    if name in friends:
+        print(" Hi " + name.title() + ", I see your favorite language is " +
+              favorite_languages[name].title() + "!")
+
+print("The following languages have been mentioned:")
+for language in set(favorite_languages.values()):
+    print(language.title())
+
+# Make an empty list for storing aliens.
+aliens = []
+
+# Make 30 green aliens.
+for alien_number in range(0, 30):
+    new_alien = {'color': 'green', 'points': 5, 'speed': 'slow'}
+    aliens.append(new_alien)
+
+for alien in aliens[0:3]:
+    if alien['color'] == 'green':
+        alien['color'] = 'yellow'
+        alien['speed'] = 'medium'
+        alien['points'] = 10
+    elif alien['color'] == 'yellow':
+        alien['color'] = 'red'
+        alien['speed'] = 'fast'
+        alien['points'] = 15
+
+# Show the first 5 aliens:
+for alien in aliens[0:5]:
+    print(alien)
+print("...")
+
+# Show how many aliens have been created.
+print("Total number of aliens: " + str(len(aliens)))
+
+# Store information about a pizza being ordered.
+pizza = {
+    'crust': 'thick',
+    'toppings': ['mushrooms', 'extra cheese'],
+    }
+
+# Summarize the order.
+print("You ordered a " + pizza['crust'] + "-crust pizza " +
+      "with the following toppings:")
+
+for topping in pizza['toppings']:
+    print("\t" + topping)
+
+users = {'aeinstein': {'first': 'albert',
+                       'last': 'einstein',
+                       'location': 'princeton'},
+         'mcurie': {'first': 'marie',
+                    'last': 'curie',
+                    'location': 'paris'},
+         }
+
+for username, user_info in users.items():
+    print("\nUsername: " + username)
+    full_name = user_info['first'] + " " + user_info['last']
+    location = user_info['location']
+
+    print("\tFull name: " + full_name.title())
+    print("\tLocation: " + location.title())
