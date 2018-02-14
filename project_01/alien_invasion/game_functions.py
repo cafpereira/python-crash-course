22af8532c9099c85ca03c93ad0398d56fb9c4f48
Carlos Pereira <carlos_pereira@ultimatesoftware.com>
2018-02-14 13:31:00 -0500

    add shoot bullets

--- project_01/alien_invasion/game_functions.py
+++ project_01/alien_invasion/game_functions.py
@@ -54,8 +54,9 @@ def update_screen(ai_settings, screen, ship, aliens, bullets):
 
     # Make the most recently drawn screen visible.
     pygame.display.flip()
-    
-def update_bullets(bullets):
+
+
+def update_bullets(ai_settings, screen, ship, aliens, bullets):
     """Update position of bullets, and get rid of old bullets."""
     # Update bullet positions.
     bullets.update()
@@ -65,9 +66,40 @@ def update_bullets(bullets):
         if bullet.rect.bottom <= 0:
             bullets.remove(bullet)
 
-def update_aliens(aliens):
+    check_bullet_alien_collisions(ai_settings, screen, ship, aliens, bullets)
+
+
+def check_bullet_alien_collisions(ai_settings, screen, ship, aliens, bullets):
+    """Respond to bullet-alien collisions."""
+    # Remove any bullets and aliens that have collided.
+    collisions = pygame.sprite.groupcollide(bullets, aliens, True, True)
+
+    if len(aliens) == 0:
+        # Destroy existing bullets, and create new fleet.
+        bullets.empty()
+        create_fleet(ai_settings, screen, ship, aliens)
+
+
+def check_fleet_edges(ai_settings, aliens):
+    """Respond appropriately if any aliens have reached an edge."""
+    for alien in aliens.sprites():
+        if alien.check_edges():
+            change_fleet_direction(ai_settings, aliens)
+            break
+
+
+def change_fleet_direction(ai_settings, aliens):
+    """Drop the entire fleet, and change the fleet's direction."""
+    for alien in aliens.sprites():
+        alien.rect.y += ai_settings.fleet_drop_speed
+    ai_settings.fleet_direction *= -1
+
+
+def update_aliens(ai_settings, aliens):
     """Update the postions of all aliens in the fleet."""
+    check_fleet_edges(ai_settings, aliens)
     aliens.update()
+
 
 def get_number_aliens_x(ai_settings, alien_width):
     """Determine the number of aliens that fit in a row."""
